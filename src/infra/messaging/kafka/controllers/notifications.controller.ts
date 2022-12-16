a9a684c2c5718b7bdec9e109bf8bf0ca70ef8112
henrique monteiro <henriquemonteiro037@gmail.com>
2022-12-16 04:40:58 +0000

    microservice development finished

--- src/infra/messaging/kafka/controllers/notifications.controller.ts
+++ src/infra/messaging/kafka/controllers/notifications.controller.ts
@@ -1,11 +1,25 @@
+import { SendNotificationUseCase } from "@app/useCases/SendNotificationUseCase";
 import { Controller } from "@nestjs/common";
-import { EventPattern } from "@nestjs/microservices";
+import { EventPattern, Payload } from "@nestjs/microservices";
+
+interface IPayload {
+    content: string
+    category: string
+    recipientId: string
+}
 
 @Controller()
 export class NotificationsController {
+    constructor(
+        private sendNotificationUseCase: SendNotificationUseCase
+    ) { }
 
     @EventPattern('notifications.send-notification')
-    async handle() {
-        console.log('notification')
+    async handle(@Payload() { category, content, recipientId }: IPayload) {
+        await this.sendNotificationUseCase.execute({
+            category,
+            content,
+            recipientId,
+        })
     }
 }
--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,12 @@
+import { SendNotificationUseCase } from "@app/useCases/SendNotificationUseCase";
+import { DatabaseModule } from "@infra/database/database.module";
 import { Module } from "@nestjs/common";
 import { NotificationsController } from "./kafka/controllers/notifications.controller";
 import { KafkaConsumerService } from "./kafka/kafka-consumer.service";
 
 @Module({
-    imports: [],
-    providers: [KafkaConsumerService],
+    imports: [DatabaseModule],
+    providers: [KafkaConsumerService, SendNotificationUseCase],
     controllers: [NotificationsController]
 })
 export class MessagingModule { }
